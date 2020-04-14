c5ec7f199a2b5d9b6a75d707c7974f04fe669509
Roland Kunz <roland.kunz@hotmail.com>
2020-04-14 14:24:53 +0200

    add spaces

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -723,9 +723,9 @@
 /**
  * Z Servo Probe, such as an endstop switch on a rotating arm.
  */
-#define SERVO0_PIN         32//13 // untested				 
-#define Z_PROBE_SERVO_NR 0   // Defaults to SERVO 0 connector.
-#define Z_SERVO_ANGLES {10,90}  // Z Servo Deploy and Stow angles
+#define SERVO0_PIN 32          //13 // untested				 
+#define Z_PROBE_SERVO_NR 0     // Defaults to SERVO 0 connector.
+#define Z_SERVO_ANGLES {10,90} // Z Servo Deploy and Stow angles
 
 /**
  * The BLTouch probe uses a Hall effect sensor and emulates a servo.
