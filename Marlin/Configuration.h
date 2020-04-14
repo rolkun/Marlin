c9d56b316ddafbaf0d0fd2ffb1b1ea8c89ff4333
Roland Kunz <roland.kunz@hotmail.com>
2020-04-14 13:43:43 +0200

    update acceleration and z probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -804,7 +804,7 @@
  // ANYCUBIC 4MAX
 #define X_PROBE_OFFSET_FROM_EXTRUDER 32     // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -2     // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.4   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.8   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 40
